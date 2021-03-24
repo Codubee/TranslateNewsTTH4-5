32be35394a6e81df8437842e241fd0838a283723
Jose Aguilar <jhagui7464@gmail.com>
2021-03-24 15:50:20 -0500

    added base explanation banner. maybe we can add a transition effect to switch between popular languages

--- frontend/src/components/Explanation.js
+++ frontend/src/components/Explanation.js
@@ -1,1 +1,20 @@
 // This is where the explanation component will be created
+
+
+import React from 'react';
+
+function ExplanationBox() {
+    return (
+        <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+          <h1 class="display-4">What is this?</h1>
+          <p class="lead">This is a project powered by students that gives you the power to translate any news article to your preferred langauge :).</p>
+        </div>
+        <div style={{ display: "flex" }}>
+        <a class="btn btn-primary btn-lg" href="#1" role="button" style={{ marginLeft: "1010px" }}>Learn more</a>
+      </div>
+      </div>
+    )
+}
+
+export default ExplanationBox;
