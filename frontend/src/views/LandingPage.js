2ad60cd2ee406522ecfea7269b7c95c3b3ae0525
codubee <info@codubee.com>
2021-03-25 14:23:22 -0500

    removed old comment and h1

--- frontend/src/views/LandingPage.js
+++ frontend/src/views/LandingPage.js
@@ -4,9 +4,7 @@ import Tools from '../components/Tools.js';
 function LandingPage(){
     return(
         <div>
-            {/* Insert your created components here */}
             <Tools></Tools>
-            <h1>Landing Page Component</h1>
         </div>
     )
 }
