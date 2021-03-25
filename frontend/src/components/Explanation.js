fd1dd1337e99b2ec194e8184abbb30cb629e35dc
Jose Aguilar <jhagui7464@gmail.com>
2021-03-25 15:59:52 -0500

    need css

--- frontend/src/components/Explanation.js
+++ frontend/src/components/Explanation.js
@@ -1,20 +1,16 @@
-// This is where the explanation component will be created
-
-
 import React from 'react';
-
-function ExplanationBox() {
+function Explanation() {
     return (
         <div class="jumbotron jumbotron-fluid">
-        <div class="container">
-          <h1 class="display-4">What is this?</h1>
-          <p class="lead">This is a project powered by students that gives you the power to translate any news article to your preferred langauge :).</p>
+            <div class="container">
+                <h1 class="display-4">What is this?</h1>
+                <p class="lead">This is a project powered by students that gives you the power to translate any news article to your preferred langauge :).</p>
+            </div>
+            <div>
+                <a class="btn btn-primary btn-lg" href="#Explanation.css" role="button" style={{ marginLeft: "1200px" }}>Learn more</a>
+            </div>
         </div>
-        <div style={{ display: "flex" }}>
-        <a class="btn btn-primary btn-lg" href="#1" role="button" style={{ marginLeft: "1010px" }}>Learn more</a>
-      </div>
-      </div>
     )
 }
 
-export default ExplanationBox;
+export default Explanation;
