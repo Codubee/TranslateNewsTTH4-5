f333c12e72f879b51b07680f9424ee1ff2672417
Jose Aguilar <jhagui7464@gmail.com>
2021-03-30 16:36:37 -0500

    removed the landing component text

--- frontend/src/views/LandingPage.js
+++ frontend/src/views/LandingPage.js
@@ -1,10 +1,9 @@
 import React from 'react';
-
+import  Explanation from '../components/Explanation.js';
 function LandingPage(){
     return(
         <div>
-            {/* Insert your created components here */}
-            <h1>Landing Page Component</h1>
+            <Explanation/>
         </div>
     )
 }
