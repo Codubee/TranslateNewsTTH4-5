1526f0193412fbf847f45ade188c8cc29722d3a8
hannah117 <hannahismail711@gmail.com>
2021-04-02 00:02:44 -0500

    added row/cols and edited css

--- frontend/src/components/Team.js
+++ frontend/src/components/Team.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {
   Card, CardImg, CardBody,
-  CardTitle, CardSubtitle, Container
+  CardTitle, CardSubtitle, Row, Col
 } from 'reactstrap';
 import Hannah from '../images/teams-pics/Hannah.jpg';
 import Lillie from '../images/teams-pics/Lillie.jpg';
@@ -22,17 +22,18 @@ function Team() {
   
   //list of teammates
   var team = [ new Teammate('Hannah ', 'Junior', 'Computer Science', 'The University of Texas at Dallas', Hannah),
-                new Teammate('Aru', 'Sophomore', 'Computer Science', 'The University of Texas at Dallas', Aru), 
+                new Teammate('Aru', 'Feshman', 'Computer Science', 'The University of Texas at Dallas', Aru), 
                 new Teammate('Kirtan', 'Junior', 'Computer Science', 'The University of Texas at Dallas', Kirtan), 
                 new Teammate('Lillie', 'Freshman', 'Computer Science', 'The University of Texas at Dallas', Lillie), 
                 new Teammate('Jose', 'Junior', 'Computer Science', 'The University of Texas at Rio Grande Valley', Jose), ];
 
   return ( 
-    <div classname='center team-card-container'> 
+    <div classname='center'> 
       <h1 className='team-header'>Meet the Team</h1>
-      <Container className="center">
+      <Row>
         {team.map((teammate, index) => (       //create card for each teammate with map function on team array
-        <Card  className="teammate-card center" key={index}>
+        <Col md='3' sm='12'>
+          <Card  className="teammate-card center" key={index}>
           <CardImg className='teammate-pic' src={teammate.pic}/>
           <CardBody className='teammate-card-body'>
             <CardTitle class="center" tag="h5">{teammate.name}</CardTitle>
@@ -41,8 +42,10 @@ function Team() {
             <CardSubtitle tag="h6" className="center mb-2 text-muted">{teammate.univ}</CardSubtitle>
           </CardBody>
         </Card>
+        </Col>
         ))}
-      </Container>
+    
+      </Row>
     </div>
 
   );
