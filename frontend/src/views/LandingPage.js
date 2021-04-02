3aa5b57da61c126eaf27e913aa089edbdef43476
Jose Aguilar <43866437+jhagui7464@users.noreply.github.com>
2021-04-02 16:50:34 -0500

    TTH-2 Added base explanation banner (#5)
    * added base explanation banner. maybe we can add a transition effect to switch between popular languages
    
    * need css
    
    * finished
    
    * removed button and removed css
    
    * removed the landing component text
    
    * Removed unused import
    
    Co-authored-by: codubee <info@codubee.com>
    Co-authored-by: Codubee <70549347+Codubee@users.noreply.github.com>

--- frontend/src/views/LandingPage.js
+++ frontend/src/views/LandingPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import  Explanation from '../components/Explanation.js';
 import Tools from '../components/Tools.js';
 import Team from '../components/Team.js';
 
 function LandingPage(){
     return(
         <div>
-            <Tools></Tools>
+            <Explanation/>
+            <Tools/>
             <Team/>
         </div>
     )
