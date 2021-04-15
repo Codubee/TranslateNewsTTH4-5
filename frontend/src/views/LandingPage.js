1fdd198b5ae5ebf15d07c62f79996a384ccc0dc6
hannah117 <hannahismail711@gmail.com>
2021-04-15 13:59:52 -0500

    language dropdown menu added

--- frontend/src/views/LandingPage.js
+++ frontend/src/views/LandingPage.js
@@ -3,11 +3,13 @@ import Features from '../components/Features';
 import  Explanation from '../components/Explanation.js';
 import Tools from '../components/Tools.js';
 import Team from '../components/Team.js';
+import Language from '../components/Language.js';
 
 function LandingPage(){
     return(
         <div>
             <Explanation/>
+            <Language/>
             <Features/>
             <Tools/>
             <Team/>
