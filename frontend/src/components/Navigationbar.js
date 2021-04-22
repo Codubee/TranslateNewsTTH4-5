677f8219ff552e68b1c62938205fbf4b32609783
Aru Gyani <gyaniaru@gmail.com>
2021-04-22 16:02:38 -0500

    TTH-7 routing (#8)
    * Initial routing implementation, updates Navigationbar Component, and creates News view
    
    * update file, component, and path names in routing to 'Translate' to match project requirements

--- frontend/src/components/Navigationbar.js
+++ frontend/src/components/Navigationbar.js
@@ -27,7 +27,7 @@ function Navigationbar(){
         <Collapse isOpen={isOpen} navbar>
           <Nav className="mr-auto" navbar>
             <NavItem>
-              <NavLink href="/">News</NavLink>
+              <NavLink href="/translate">Translate</NavLink> 
             </NavItem>
           </Nav>
           <NavbarText><i class="fas fa-language"></i></NavbarText>
