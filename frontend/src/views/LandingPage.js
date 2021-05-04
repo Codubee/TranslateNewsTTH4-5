f3599e67ea7db61683b906fc421681809c7e666e
Jose Aguilar <jhagui7464@gmail.com>
2021-05-04 17:15:49 -0500

    forgot about style sheet

--- frontend/src/views/LandingPage.js
+++ frontend/src/views/LandingPage.js
@@ -3,12 +3,14 @@ import Features from '../components/Features';
 import  Explanation from '../components/Explanation.js';
 import Tools from '../components/Tools.js';
 import Team from '../components/Team.js';
+import NewsDisplay from '../components/NewsDisplay.js';
 
 function LandingPage(){
     return(
         <div>
             <Explanation/>
             <Features/>
+            <NewsDisplay/>
             <Tools/>
             <Team/>
         </div>
