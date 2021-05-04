ccb24c2516aed31e8b3d7fc4c90f562ebc0dd3df
Jose Aguilar <jhagui7464@gmail.com>
2021-05-04 15:43:15 -0500

    got img to show

--- frontend/src/components/NewsDisplay.js
+++ frontend/src/components/NewsDisplay.js
@@ -1,11 +1,17 @@
 import React from 'react';
 function NewsDisplay() {
-    const data =[{"image":"imageurl",
-    "title":"news title",
-    "subtext":"some subtext",
+    const data =[{"image":"https://blog.cdn.own3d.tv/resize=fit:crop,height:400,width:600/1yUTadzQTDWnMKQunV2T",
+    "title":"Peepo is Happy",
+    "subtext":"Happiness has reached the Peepo and now he will become wide",
     "url":"news article url"
     }];
-    const listItems = data.map((d) => <li key={d.name}>{d.name}</li>);
+    const listItems = data.map((d) => <li key={d.title}>{
+        <img 
+        src={d.image}
+        alt= {d.url}
+        width="300" height="300"
+        />
+        } </li>);
 
     return (
       <div>
