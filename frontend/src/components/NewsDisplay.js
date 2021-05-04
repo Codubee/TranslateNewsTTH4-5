f43fce528394fa6ce9b0e55512a1e35f6ef559c5
Jose Aguilar <jhagui7464@gmail.com>
2021-05-04 16:20:56 -0500

    finalized base component, waiting feedback

--- frontend/src/components/NewsDisplay.js
+++ frontend/src/components/NewsDisplay.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import "../styles/NewsDisplay.css";
+
 function NewsDisplay() {
     const data =[{"image":"https://blog.cdn.own3d.tv/resize=fit:crop,height:400,width:600/1yUTadzQTDWnMKQunV2T",
     "title":"Peepo is Happy",
     "subtext":"Happiness has reached the Peepo and now he will become wide",
-    "url":"news article url"
+    "url":"https://youtu.be/accF0gAmdeg"
     }];
     const listItems = data.map((d) => <li key={d.title}>{
-        <img 
-        src={d.image}
-        alt= {d.url}
-        width="300" height="300"
-        />
+       
+        <ul >
+            <h1><a href={d.url}>{d.title}</a></h1>
+
+            <li> <img src={d.image} alt= {d.name}/> </li>
+            <li> {d.subtext} </li>
+        </ul>
         } </li>);
 
     return (
