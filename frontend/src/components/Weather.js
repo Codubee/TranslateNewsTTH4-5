10e83c19fd6521adfb27db6d0890040b8fdca738
lilliemcmaster <lillie.mcmaster@gmail.com>
2021-05-18 16:34:52 -0500

    API call for getWeather

--- frontend/src/components/Weather.js
+++ frontend/src/components/Weather.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import axois from 'axios';
 import '../styles/Weather.css';
-function Weather(props){
-    return(
-        <div className="weather">
-            <p className = "font-weight-bold">Today's Weather in {props.city} </p> 
+
+class Weather extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            city: "Dallas",
+            temp: ""
+        }
+    }
+
+    componentDidMount(){
+          axois.get("/getWeather")//check this line need to use the 
+          .then((response)=> {
+              console.log(response.data);
+
+              this.setState({
+                  temp: response.data.temp
+              })
+          })
+    }
+
+    render(){
+        return(
+            <div className="weather">
+            <p className = "font-weight-bold">Today's Weather in {this.state.city} </p> 
             <div className ="temp">
-                <p className="font-weight-light">{props.temp}</p>
+                <p className="font-weight-light">{this.state.temp}</p> 
             </div>
         </div>
-    )
+        )
+    }
 }
 export default Weather;
