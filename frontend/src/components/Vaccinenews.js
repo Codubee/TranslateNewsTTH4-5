ab18b4451afb7aff1677e20c07bb3055d685405f
kirtanmedi <kirtanmedi@gmail.com>
2021-05-20 16:08:51 -0500

    final

--- frontend/src/components/Vaccinenews.js
+++ frontend/src/components/Vaccinenews.js
@@ -4,7 +4,7 @@ import {
     CardTitle, CardSubtitle, Row, Col
 } from 'reactstrap';
 import '../styles/Vaccinenews.css';
-import VaccineNews from '../data/vaccinenews.json'
+import VaccineNews from '../data/vaccinenews.json';
 
 function Vaccinenews(){
     return(
@@ -20,7 +20,6 @@ function Vaccinenews(){
                             <CardBody>
                             <CardTitle tag="h5"><a target='_blank' href={vaccineCard.url}>{vaccineCard.title}</a></CardTitle>
                             <CardSubtitle tag="h6" className="mb-2 text-muted">{vaccineCard.subtext}</CardSubtitle>
-                            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
                             </CardBody>
                         </Card>
                     </Col>
